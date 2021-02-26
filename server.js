db7555285dfa585e294cde40f5d78cdf0bb4e48e
Elias Calagiu <eliasc81@gmail.com>
2021-02-26 12:48:10 -0800

    added server boilerplate

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
 const express = require("express")
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// serve content from the public directory 
+app.use(express.static("public"));
+
+// parse application body as JSON
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+// set handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+// import routes and give the server access to them
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// start the server to listen for client requests
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
