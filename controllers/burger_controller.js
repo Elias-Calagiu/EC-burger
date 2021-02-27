4e97c095061b99af892925dcec6a7a83ef9dd7a8
Elias Calagiu <eliasc81@gmail.com>
2021-02-26 16:13:18 -0800

    wrote delete route for burger controller

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -47,3 +47,19 @@ router.put("/api/burgers", function(req,res){
         }
     })
 })
+
+// Route to delete burger entry
+
+router.delete("/api/burgers/:id", function(req,res){
+    var condition = "id = " + req.params.id
+
+    burger.delete(condition, function(result){
+        if (result.affectedRows == 0){
+            return res.status(404).end()
+        } else {
+            res.status(200).end();
+        }
+    })
+})
+
+module.exports = router
